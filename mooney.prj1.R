87abf95b553a4aa802aeb29874dab4be4f25fa00
Stephen Mooney <stephenmooney@stephens-mbp-3.bc.edu>
2022-09-11 16:25:49 -0400

    Stephen Mooney Project one

---
+++ mooney.prj1.R
@@ -0,0 +1,36 @@
+#loading libraries and setting the working directory
+library(ggplot2)
+library(tidyverse)
+setwd("~/Documents/RStudio/scales")
+
+#loading scales dataset
+dat <- read.csv("scales.csv")
+
+#reporting the class of each column
+sapply(dat,class)
+
+#reporting dimensions of the dataset, reported as "rows, columns"
+dim(dat)
+
+#summary of number of scales punctured for each species
+dat$species <- as.factor(dat$species)
+dat %>%
+  group_by(species) %>%
+  summarise(n = n())
+
+#summary of number of specimens sampled for each species
+dat %>% 
+  count(species,specimen) %>%
+  print() %>%
+  count(species,name = "n.specimens")
+
+#PDF countaing boxplots of puncture force and quadrant
+species <- levels(dat$species)
+pdf("stephenmooney.species.quadrant.pdf")
+for(i in species){
+  p <- dat %>%
+    filter(species==i)%>%
+    ggplot()+geom_boxplot(aes(x=quadrant,y=N))+ggtitle(i)
+  print(p)
+}
+dev.off()

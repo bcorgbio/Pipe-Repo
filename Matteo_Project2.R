7ea7d8c9fa644ca380161075b553dffd62ee6ff4
torquatm <torquatm@bc.edu>
2022-09-16 12:29:40 -0400

    Matteo Project 2

--- Matteo_Project2.R
+++ Matteo_Project2.R
@@ -7,3 +7,23 @@ speeds <- read_csv("pseed.calibration.csv")
 pseed2 <- pseed%>%
   left_join(speeds,by=c("speed"="vol"))%>%
   print()
+
+pseed.bl%>%
+  print()
+
+pseed2%>%
+  select(fish)%>%
+  unique()
+
+pseed2 <- pseed2%>%
+  left_join(pseed.bl,by="fish")%>%
+  print()
+
+
+pseed2 <- pseed2%>%
+  mutate(bl.s=cm.s/bl)%>%
+  print()
+
+pseed2%>%
+  filter(date=="2019-06-17-151149", fin=="L")%>%
+  ggplot(aes(x=frame,y=amp.bl))+geom_point()
